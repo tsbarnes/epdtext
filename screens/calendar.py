c619ff7197407b25150374b37ff348acd6d9efeb
Thea Barnes <thea@tsbarnes.com>
2021-09-07 23:14:21 -0400

    Started working on supporting CalDAV

--- screens/calendar.py
+++ screens/calendar.py
@@ -33,16 +33,12 @@ def get_events_from_caldav(url, username, password):
     client = caldav.DAVClient(url=url, username=username, password=password)
     principal = client.principal()
     calendars = principal.calendars()
-    print(calendars)
 
     for calendar in calendars:
         calendar_events = calendar.date_search(start=datetime.today(), end=datetime.today() + timedelta(days=7),
                                                expand=True)
         for event in calendar_events:
-            try:
-                text += '\t' + humanize.naturalday(event.start) + '\n'
-            except AttributeError:
-                text += '\tNo start date\n'
+            text += '\t' + humanize.naturalday(event.start) + '\n'
             summary = event.summary.replace('\n', ' ')
             lines = textwrap.wrap(summary, width=28)
             for line in lines:
