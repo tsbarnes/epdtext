4d85ca26dc3f0d1d2f670b4d4efb628169deaa98
Thea Barnes <thea@tsbarnes.com>
2021-09-14 08:43:00 -0400

    Bug fixes, documentation

--- libs/calendar.py
+++ libs/calendar.py
@@ -12,28 +12,47 @@ from settings import CALENDAR_URLS, TIMEZONE
 
 def sort_by_date(obj: dict):
     """
-
-    :param obj:
-    :return:
+    Sort the events or tasks by date
+    :param obj: dict containing sumary and start/due date
+    :return: the same object, with time added if needed
     """
-    if isinstance(obj["start"], date) and not isinstance(obj["start"], datetime):
-        return datetime.combine(obj["start"], datetime.min.time())
-    return obj["start"]
+    if obj.get("start"):
+        if isinstance(obj["start"], date) and not isinstance(obj["start"], datetime):
+            return datetime.combine(obj["start"], datetime.min.time())
+        return obj["start"]
+    elif obj.get("due"):
+        if isinstance(obj["due"], date) and not isinstance(obj["due"], datetime):
+            return datetime.combine(obj["due"], datetime.min.time())
+        return obj["due"]
+    else:
+        raise ValueError("Object has no start or due date")
 
 
 class Calendar:
+    """
+    This class handles the calendar events and tasks
+    """
     timezone: tzinfo = None
     refresh_interval: int = settings.CALENDAR_REFRESH
     events: list = []
     tasks: list = []
 
     def __init__(self):
+        """
+        Initialize the timezone
+        """
         if isinstance(TIMEZONE, tzinfo):
             self.timezone = TIMEZONE
         else:
             self.timezone = pytz.timezone(TIMEZONE)
 
     def standardize_date(self, arg, ignore_timezone=False):
+        """
+        Adds time to dates to make datetimes as needed
+        :param arg: an object containing a summary and date
+        :param ignore_timezone: if True, ignore timezones
+        :return: a new datetime object, or the same object if no changes were needed
+        """
         if isinstance(arg, datetime) and (not arg.tzinfo or ignore_timezone):
             return datetime.combine(arg.date(), arg.time(), self.timezone)
         elif isinstance(arg, date) and not isinstance(arg, datetime):
@@ -43,6 +62,11 @@ class Calendar:
             return arg
 
     def get_events_from_webcal(self, url):
+        """
+        Retrieve events from webcal and append them to the list
+        :param url: the URL of the webcal
+        :return: list of events
+        """
         try:
             timeline: list = events(url)
             for event in timeline:
@@ -60,6 +84,13 @@ class Calendar:
         return self.events
 
     def get_events_from_caldav(self, url, username, password):
+        """
+        Retrieve events and tasks from CalDAV
+        :param url: URL of CalDAV server
+        :param username: CalDAV user name
+        :param password: CalDAV password
+        :return: the list of events
+        """
         try:
             client = caldav.DAVClient(url=url, username=username, password=password)
             principal = client.principal()
@@ -96,10 +127,13 @@ class Calendar:
                     'due': due,
                     'summary': summary
                 })
-
         return self.events
 
     def get_latest_events(self):
+        """
+        Force updare of evetnts]
+        :return:
+        """
         logging.debug("Started reading calendars...")
         self.events = []
         self.tasks = []
@@ -120,6 +154,10 @@ class Calendar:
         return self.events
 
     def events_as_string(self):
+        """
+        Get the current events as a string
+        :return: list of events
+        """
         text = ''
 
         for obj in self.events:
@@ -133,6 +171,10 @@ class Calendar:
         return text
 
     def tasks_as_string(self):
+        """
+        Get the current tasks as a string
+        :return: list of tasks
+        """
         text = ''
 
         for obj in self.tasks:
@@ -147,8 +189,16 @@ calendar = Calendar()
 
 
 def get_calendar():
+    """
+    Retrieve main Calendar object
+    :return: Calendar
+    """
     return calendar
 
 
 def update_calendar():
+    """
+    Update calendar events and tasks
+    :return: None
+    """
     calendar.get_latest_events()
--- libs/weather.py
+++ libs/weather.py
@@ -3,10 +3,17 @@ import settings
 
 
 class Weather:
+    """
+    This class provides access to the weather info
+    """
     weather = None
     refresh_interval: int = settings.WEATHER_REFRESH
 
     async def update(self):
+        """
+        Update the weather info
+        :return: None
+        """
         client = python_weather.Client(format=settings.WEATHER_FORMAT)
         self.weather = await client.find(settings.WEATHER_CITY)
         await client.close()
@@ -16,8 +23,16 @@ weather: Weather = Weather()
 
 
 def get_weather():
+    """
+    Get the main weather object
+    :return: Weather
+    """
     return weather
 
 
 def update_weather():
+    """
+    Update the weather info
+    :return: None
+    """
     weather.update()
